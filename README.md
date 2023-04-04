0966cbcdccf7a3cfe9278f9bbd6fb900ab2546d1
Charlie Chenyu Zhang <charlie.chenyu.zhang@gmail.com>
2023-04-04 03:16:12 -0400

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# miracleplus
-miracleplus
+# how to run the project
+
+step 1: install packages by running
+
+```
+npm ci
+```
+
+step 2: create a .env file with your ChatGPT token
+REACT_APP_MIRACLEPLUS_API_KEY="<your token>"
+
+step 3: run the following script to run
+
+```
+npm run start
+```
---
+++ src/MiraclePlusCopilot2.js
@@ -0,0 +1,207 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import logo from "./miracleplus-logo.png";
+
+// import betaPortfolios from "../assets/portfolios/beta-updated-portfolio.png";
+import TextField from "@mui/material/TextField";
+import CircularProgress from "@mui/material/CircularProgress";
+import { Configuration, OpenAIApi } from "openai";
+
+const Header = styled.div`
+  margin-top: 3rem;
+`;
+
+const Logo = styled.img`
+  width: 10.5rem;
+  height: 7rem;
+  /* position: fixed; */
+`;
+
+const PortfolioImg = styled.img`
+  width: 100%;
+  /* height: 30rem; */
+  /* position: fixed; */
+`;
+
+const TweetsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  height: 400px;
+  overflow: scroll;
+  margin: auto;
+`;
+
+const Button = styled.a`
+  margin-top: 1rem;
+  font-weight: 700;
+  font-size: 1.125rem;
+  border: solid 1px;
+  cursor: pointer;
+  padding: 0.25rem 0.75rem;
+  width: fit-content;
+
+  transition: 0.5s;
+  background-size: 200% auto;
+  color: white;
+  /* text-shadow: 0px 0px 10px rgba(0,0,0,0.2);*/
+  box-shadow: 0 0 20px #eee;
+  border-radius: 10px;
+  pointer-events: ${(props) => (props.disabled ? "none" : "auto")};
+
+  background-image: ${(props) =>
+    props.disabled
+      ? `linear-gradient(
+      to right,
+      lightgrey 0%,
+      grey 51%,
+      grey 100%
+    )`
+      : `linear-gradient(
+    to right,
+    #f6d365 0%,
+    #fda085 51%,
+    #f6d365 100%
+  )`};
+
+  :hover {
+    background-position: right center;
+    text-decoration: none;
+  }
+`;
+
+const configuration = new Configuration({
+  apiKey: process.env.REACT_APP_MIRACLEPLUS_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const MiraclePlusCopilot2 = () => {
+  const [sortingCriteria, setSortingCriteria] = useState({
+    impression_count: false,
+    like_count: true,
+    quote_count: false,
+    reply_count: false,
+    retweet_count: false,
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [isTwitterLoading, setIsTwitterLoading] = useState(false);
+  const [isSummarizationLoading, setIsSummarizationLoading] = useState(false);
+  const [pickedTweets, setPickedTweets] = useState({});
+  const [portfolios, setPortfolios] = useState(
+    "I am a master's degree candidate in Computer Science at Peking University. I am interested in social network and want to build something related to the virtual networking platform. "
+  );
+  // const [showPortfolios, setShowPortfolios] = useState(false);
+  const [wholeEmail, setWholeEmail] = useState("");
+  const [ideas, setIdeas] = useState(""); // FIXME: update the ideas once I fetch the data from twitter
+  const [selectedTweets, setSelectedTweets] = useState("");
+
+  const handleChange = (event) => {
+    setSortingCriteria({
+      ...sortingCriteria,
+      [event.target.name]: event.target.checked,
+    });
+  };
+
+  const error = Object.values(sortingCriteria).filter((v) => v).length < 1;
+
+  const callOpenAI = async (prompt) => {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+    });
+    return completion;
+  };
+
+  // input: {Instacart: [], Rippling: []}
+  // output: Instacart has tweeted ... Rippling has tweeted ...
+  async function generateIdeas() {
+    let onelineSummary;
+    setIsLoading(true);
+    try {
+      // it saved the previous convo between the AI and bot
+      const prompt = `Act as my co-founder. Your task is to generate a business plan for the startup based on my background information. The business plan should include an idea name, a short one liner, why me, target user persona, user's pain points to solve, main value propositions, sales &amp; marketing channels, revenue stream sources, cost structures, key activities, key resources, key partners, idea validation steps, estimated 1st year cost of operation, and potential business challenges to look for. 
+
+          Background information: ${portfolios} 
+          
+          """ 
+          
+          Write the result in a html table.`;
+      const res = await callOpenAI(prompt);
+      onelineSummary = res.data.choices[0].message.content;
+    } catch (error) {
+      // Consider adjusting the error handling logic for your use case
+      if (error.response) {
+        console.error(error.response.status, error.response.data);
+      } else {
+        console.error(`Error with OpenAI API request: ${error.message}`);
+      }
+    }
+
+    setIsLoading(false);
+    return onelineSummary;
+  }
+
+  useEffect(() => {
+    document.getElementsByTagName("title")[0].text = "Beta Fellowship";
+  });
+
+  return (
+    <div>
+      <div className="container">
+        <Header>
+          <Logo src={logo} alt="listening"></Logo>
+
+          <div style={{ marginTop: "50px" }}>
+            The easiest way to match what you can, what you want and what the
+            world needs.
+          </div>
+          <div style={{ margin: "50px 0" }}>
+            <TextField
+              id="outlined-multiline-static"
+              label="introduce yourself"
+              multiline
+              fullWidth
+              rows={4}
+              defaultValue="Default Value"
+              value={portfolios}
+              onChange={(e) => {
+                setPortfolios(e.target.value);
+              }}
+            />
+          </div>
+
+          <Button
+            disabled={isLoading ? true : false}
+            onClick={async () => {
+              const ideas = await generateIdeas();
+              setIdeas(ideas);
+            }}
+          >
+            generate ideas
+          </Button>
+
+          <div
+            style={{
+              minHeight: "300px",
+              marginBottom: "100px",
+              marginTop: "50px",
+            }}
+          >
+            <div
+              dangerouslySetInnerHTML={{
+                __html: ideas,
+              }}
+            />
+
+            {isLoading && <CircularProgress style={{ marginTop: "50px" }} />}
+          </div>
+        </Header>
+      </div>
+    </div>
+  );
+};
+
+export default MiraclePlusCopilot2;
