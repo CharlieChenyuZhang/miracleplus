ecb743960b4dedc9a5bbe6ffe2d0be50794e8acd
Shamiya Lin <75193263+shamiya829@users.noreply.github.com>
2023-04-06 11:18:02 -0500

    Update MiraclePlusCopilot2.js

--- src/MiraclePlusCopilot2.js
+++ src/MiraclePlusCopilot2.js
@@ -1,74 +1,55 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import logo from "./miracleplus-logo.png";
-
-// import betaPortfolios from "../assets/portfolios/beta-updated-portfolio.png";
 import TextField from "@mui/material/TextField";
 import CircularProgress from "@mui/material/CircularProgress";
 import { Configuration, OpenAIApi } from "openai";
 
 const Header = styled.div`
-  margin-top: 3rem;
+  margin-top: 10rem;
 `;
 
 const Logo = styled.img`
-  width: 10.5rem;
+  width: 7rem;
   height: 7rem;
-  /* position: fixed; */
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
 `;
 
-const PortfolioImg = styled.img`
-  width: 100%;
-  /* height: 30rem; */
-  /* position: fixed; */
-`;
-
-const TweetsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  height: 400px;
-  overflow: scroll;
-  margin: auto;
-`;
-
-const Button = styled.a`
-  margin-top: 1rem;
+const Button = styled.button`
+  background-color: #017dfe;
+  border: none;
+  border-radius: 30px;
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
+  color: white;
+  cursor: pointer;
+  display: inline-block;
   font-weight: 700;
   font-size: 1.125rem;
-  border: solid 1px;
-  cursor: pointer;
-  padding: 0.25rem 0.75rem;
-  width: fit-content;
-
-  transition: 0.5s;
-  background-size: 200% auto;
-  color: white;
-  /* text-shadow: 0px 0px 10px rgba(0,0,0,0.2);*/
-  box-shadow: 0 0 20px #eee;
-  border-radius: 10px;
-  pointer-events: ${(props) => (props.disabled ? "none" : "auto")};
-
-  background-image: ${(props) =>
-    props.disabled
-      ? `linear-gradient(
-      to right,
-      lightgrey 0%,
-      grey 51%,
-      grey 100%
-    )`
-      : `linear-gradient(
-    to right,
-    #f6d365 0%,
-    #fda085 51%,
-    #f6d365 100%
-  )`};
-
-  :hover {
-    background-position: right center;
-    text-decoration: none;
+  padding: 0.5rem 0.75rem;
+  line-height: 50px;
+  text-align: center;
+  text-decoration: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  position: relative;
+  overflow: hidden;
+  perspective: 1000px;
+  transition: 0.3s;
+  &:hover {
+    box-shadow: 0 6px 24px rgba(0, 0, 0, 0.3);
+  }
+  &:active {
+    transform: translateY(1px) rotateX(0deg);
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+  }
+  @media (min-width: 768px) {
+    min-width: 120px;
+    padding: 0 25px;
+    font-size: 18px;
+    line-height: 50px;
   }
 `;
 
@@ -79,33 +60,9 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const MiraclePlusCopilot2 = () => {
-  const [sortingCriteria, setSortingCriteria] = useState({
-    impression_count: false,
-    like_count: true,
-    quote_count: false,
-    reply_count: false,
-    retweet_count: false,
-  });
   const [isLoading, setIsLoading] = useState(false);
-  const [isTwitterLoading, setIsTwitterLoading] = useState(false);
-  const [isSummarizationLoading, setIsSummarizationLoading] = useState(false);
-  const [pickedTweets, setPickedTweets] = useState({});
-  const [portfolios, setPortfolios] = useState(
-    "I am a master's degree candidate in Computer Science at Peking University. I am interested in social network and want to build something related to the virtual networking platform. "
-  );
-  // const [showPortfolios, setShowPortfolios] = useState(false);
-  const [wholeEmail, setWholeEmail] = useState("");
-  const [ideas, setIdeas] = useState(""); // FIXME: update the ideas once I fetch the data from twitter
-  const [selectedTweets, setSelectedTweets] = useState("");
-
-  const handleChange = (event) => {
-    setSortingCriteria({
-      ...sortingCriteria,
-      [event.target.name]: event.target.checked,
-    });
-  };
-
-  const error = Object.values(sortingCriteria).filter((v) => v).length < 1;
+  const [portfolios, setPortfolios] = useState("Enter text here...");
+  const [ideas, setIdeas] = useState("");
 
   const callOpenAI = async (prompt) => {
     const completion = await openai.createChatCompletion({
@@ -115,24 +72,58 @@ const MiraclePlusCopilot2 = () => {
     return completion;
   };
 
-  // input: {Instacart: [], Rippling: []}
-  // output: Instacart has tweeted ... Rippling has tweeted ...
   async function generateIdeas() {
+    console.log("ideas generated");
     let onelineSummary;
     setIsLoading(true);
     try {
-      // it saved the previous convo between the AI and bot
-      const prompt = `Act as my co-founder. Your task is to generate a business plan for the startup based on my background information. The business plan should include an idea name, a short one liner, why me, target user persona, user's pain points to solve, main value propositions, sales &amp; marketing channels, revenue stream sources, cost structures, key activities, key resources, key partners, idea validation steps, estimated 1st year cost of operation, and potential business challenges to look for. 
+      const prompt = 
+      `Task: Generate potential business ideas based on an individual's background 
+       and relevant industries, taking into account location factors and recent 
+       developments and opportunities. Evaluate the feasibility of each idea, considering 
+       factors such as product market fit, people mission fit, market potential, TAM, 
+       cost, fundraising needs, and any other relevant factors.
+      Output: An HTML table with each potential idea and a score out of 100 for its 
+      feasibility, along with an explanation of the score and the metrics used to evaluate it.
+      For each metric, make it out of 100. You don't have to use whole numbers. Provide a
+      reasoning for each of the metrics instead of only providing a number. Include
+      information of what types of co-founders the user should be looking for and 
+      where they may find them. 
+      If the resume inputted was in Chinese, generate the results in Chinese. Same with
+      other languages.
+      Follow up under the ideas section with a personal analysis table with strength and
+      weakenesses, past experiences, storytelling, and any additional factors that my be relevant.
+      After that, with a 30px margin top: Create visuals at the end of the 
+      table for the personal analysis. Use a gradient that starts from white 
+      and ends at (#017dfe). Use an arrow with label and a number indicator to point out 
+      where each lands. Make sure the number lands on the bar.
+      
+      AT THE VERY END OF THE DOC with a 30px margin top: Create visuals at the end of the 
+      table for relevance of the problem, usefullness of the solution, uniqueness, virality, 
+      and willingness to buy using a light blue theme. Use a gradient that starts from white 
+      and ends at (#017dfe). Use an arrow with label and a number indicator to point out 
+      where each lands. Make sure the number lands on the bar.
+      Refer to the user by their first name and use their resume (${portfolios}) to inform the evaluation.
+      Multiple rows should be provided for multiple potential ideas.
+      For each idea, evaluate its feasibility and include a brief explanation of the factors 
+      that contribute to the feasibility score.
+      The column title of the result table should be bold and have a light blue background (#017dfe).
+      Make sure the text that is on white background does not blend in with the background.
+      Make the visual aesthetically pleasing with good spacing.`;
 
-          Background information: ${portfolios} 
-          
-          """ 
-          
-          Write the result in a html table.`;
       const res = await callOpenAI(prompt);
       onelineSummary = res.data.choices[0].message.content;
+
+      // Create a blob object from the HTML content
+      const blob = new Blob([onelineSummary], { type: "text/html" });
+      const url = URL.createObjectURL(blob);
+
+      // Create a download link and click it programmatically to download the file
+      const a = document.createElement("a");
+      a.download = "result.html";
+      a.href = url;
+      a.click();
     } catch (error) {
-      // Consider adjusting the error handling logic for your use case
       if (error.response) {
         console.error(error.response.status, error.response.data);
       } else {
@@ -152,16 +143,24 @@ const MiraclePlusCopilot2 = () => {
     <div>
       <div className="container">
         <Header>
-          <Logo src={logo} alt="listening"></Logo>
-
-          <div style={{ marginTop: "50px" }}>
-            The easiest way to match what you can, what you want and what the
-            world needs.
+          <Logo src={logo} alt="listening" />
+          <div style={{ marginTop: "50px", textAlign: "center" }}>
+            <strong>
+              Unleash your unique abilities, ignite your deepest passions, and
+              together, let's transform the world.
+            </strong>
           </div>
-          <div style={{ margin: "50px 0" }}>
+
+          <div
+            style={{
+              margin: "50px 0",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
             <TextField
               id="outlined-multiline-static"
-              label="introduce yourself"
+              label="Drop your resume"
               multiline
               fullWidth
               rows={4}
@@ -173,15 +172,17 @@ const MiraclePlusCopilot2 = () => {
             />
           </div>
 
-          <Button
-            disabled={isLoading ? true : false}
-            onClick={async () => {
-              const ideas = await generateIdeas();
-              setIdeas(ideas);
-            }}
-          >
-            generate ideas
-          </Button>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <Button
+              disabled={isLoading ? true : false}
+              onClick={async () => {
+                const ideas = await generateIdeas();
+                setIdeas(ideas);
+              }}
+            >
+              Generate Ideas
+            </Button>
+          </div>
 
           <div
             style={{
